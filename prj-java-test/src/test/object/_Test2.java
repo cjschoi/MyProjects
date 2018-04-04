02bae97dee970bc321b2f6c83dfc6a561c36b0ce
cjs <cjs@cjs-PC>
2018-04-04 11:28:17 +0900

    opp[p[

---
+++ prj-java-test/src/test/object/_Test2.java
@@ -0,0 +1,9 @@
+package test.object;
+
+
+public class _Test2  {
+	public static void main(String[] args){
+ 
+    }
+}
+
---
+++ prj-java-test/src/test/socket2/test/BulkClientTester.java
@@ -0,0 +1,34 @@
+package test.socket2.test;
+
+import test.socket2.Client;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class BulkClientTester {
+
+	public static void main(String[] args) throws IOException {
+
+		int port = Integer.parseInt(args[0]);
+
+		//int numberOfClient = 10;
+		int numberOfClient = 1;
+		ArrayList<Client> clients = new ArrayList<Client>();
+		for (int i = 0; i < numberOfClient; i++) {
+			Client client = new Client(port);
+			client.startClientDaemon();
+			clients.add(client);
+		}
+
+		//for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 1; i++) {	
+			int j = 0;
+			for (Client client : clients) {
+				String toSend = "TEST_" + j + "_" + i;
+				String result = client.write(toSend);
+				System.out.println(toSend + "=" + " " + result.length() + "," + result);
+				j++;
+			}
+		}
+	}
+}
